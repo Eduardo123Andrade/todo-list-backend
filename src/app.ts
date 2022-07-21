2f001a6fb8bc92a40fea086e5a636e3058c38ffc
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2022-07-21 11:32:49 -0300

    fixed connection

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import "express-async-errors";
-import "./database/connection";
+import createConnection from "./database/connection";
 import { router } from "./routes/index";
 import Cors from "cors";
 import { errorHandler } from "./errors/handler";
 import { corsConfig } from "./config/cors.config";
+import { environmentKeys } from "./config";
+
+createConnection()
+    .then(() => {
+        if (environmentKeys.nodeEnv !== "test")
+            console.log('Connected to DB')
+    })
+    .catch((error) => console.log("Something wrong", error));
 
 const app = express();
 
