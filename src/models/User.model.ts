1af72a28aadd75eba2ed13f6ecffbe0e4909402b
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2022-07-21 10:17:49 -0300

    created user model

---
+++ src/models/User.model.ts
@@ -0,0 +1,23 @@
+import { Column, PrimaryGeneratedColumn } from "typeorm"
+
+
+export class User {
+  
+  @PrimaryGeneratedColumn('uuid')
+  readonly id: string
+  
+  @Column()  
+  name: string
+  
+  @Column()  
+  email: string
+  
+  @Column()  
+  password: string
+  
+  @Column({name: 'create_at', default: Date.now()})  
+  createAt: string
+  
+  @Column({name: 'update_at', default: Date.now()})  
+  updateAt: string
+}
