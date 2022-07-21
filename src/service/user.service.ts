e54a9b5ad7f262b2fdd5cbfeb4f2d2ce30c8c9b7
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2022-07-21 10:46:24 -0300

    created user service

---
+++ src/service/user.service.ts
@@ -0,0 +1,95 @@
+import { UserWithoutIdAndPassword } from './../interface/user.interface';
+import { User } from './../models/User.model';
+import { getRepository } from "typeorm"
+import { UserInitialData } from 'src/interface';
+import { encoder } from 'src/utils';
+
+
+const createUser = async (userData: UserInitialData) => {
+  const repository = getRepository(User)
+
+  const password = await encoder.codify(userData.password)
+  const userCreated = repository.create({ ...userData, password })
+
+  const user = await repository.save(userCreated)
+
+  return user
+}
+
+
+const findUserByEmail = async (email: string) => {
+  const repository = getRepository(User)
+  const user = await repository.findOne({ where: { email } })
+
+  return user
+}
+
+const findUserById = async (id: string) => {
+  const repository = getRepository(User)
+  const user = await repository.findOne({ where: { id } })
+
+  if (!user)
+    throw new Error()
+
+  return user
+}
+
+const updateUser = async (id: string, userData: UserWithoutIdAndPassword) => {
+  const repository = getRepository(User)
+  const findingUser = await findUserById(id)
+
+  Object.assign(findingUser, userData)
+
+  const updatedUser = await repository.save(findingUser)
+
+  return updatedUser
+}
+
+const listUsers = async () => {
+  const repository = getRepository(User)
+  const users = repository.find()
+
+  return users
+}
+
+
+const deleteUser = async (id: string) => {
+  const user = await findUserById(id);
+
+  await getRepository(User).remove(user);
+
+  return true;
+}
+
+
+const updatePassword = async (newPassword: string, oldPassword: string, user?: UserData,) => {
+  const repository = getRepository(User)
+
+  if (!user)
+    // throw new NotFoundError(ErrorMessage.USER_NOT_FOUND)
+    throw new Error()
+
+  const isRightPassword =
+    await encoder.verifyPassword(oldPassword, user.password)
+
+  if (!isRightPassword)
+    // throw new InvalidPasswordError()
+    throw new Error()
+
+  const password = await encoder.codify(newPassword)
+
+  Object.assign(user, { password })
+
+  const updatedUser = await repository.save(user)
+
+  return updatedUser
+}
+
+export const UserService = {
+  createUser,
+  findUserById,
+  updateUser,
+  listUsers,
+  deleteUser,
+  updatePassword
+}
